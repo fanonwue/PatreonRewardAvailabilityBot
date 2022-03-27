48ca5d3a779e58f3d49c04c8f52ec53d0076f5ed
Fabian Wünderich <fabian@wuenderich.de>
2022-03-27 17:31:32 +0200

    Put ktorVersion directly into the dependency string for Dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,22 +15,18 @@ repositories {
     mavenCentral()
 }
 
-val ktorVersion: String by project
-
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+
+    implementation("io.ktor:ktor-client-core:1.6.8")
+    implementation("io.ktor:ktor-client-cio:1.6.8")
+    implementation("io.ktor:ktor-client-serialization:1.6.8")
 
     implementation("org.telegram:telegrambots:5.7.1")
     implementation("org.telegram:telegrambots-abilities:5.7.1")
 
-    // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-slf4j18-impl
     implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.17.2")
-
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
