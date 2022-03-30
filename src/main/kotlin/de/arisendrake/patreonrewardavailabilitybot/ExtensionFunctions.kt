1f1a26fc93392f1c539b41baa7a073b4e8db924e
Fabian Wünderich <fabian@wuenderich.de>
2022-03-30 07:11:45 +0200

    removeAll helper extension function

--- src/main/kotlin/de/arisendrake/patreonrewardavailabilitybot/ExtensionFunctions.kt
+++ src/main/kotlin/de/arisendrake/patreonrewardavailabilitybot/ExtensionFunctions.kt
@@ -2,3 +2,11 @@ package de.arisendrake.patreonrewardavailabilitybot
 
 fun String.withoutNewlines() = this
     .replace("\\r\\n|\\n".toRegex(), " ")
+
+fun <T, K> MutableMap<T, K>.removeAll(iterable: Iterable<T>) = removeAll(iterable.iterator())
+
+fun <T, K> MutableMap<T, K>.removeAll(sequence: Sequence<T>) = removeAll(sequence.iterator())
+
+fun <T, K> MutableMap<T, K>.removeAll(iterator: Iterator<T>) = iterator.forEach {
+    this.remove(it)
+}
