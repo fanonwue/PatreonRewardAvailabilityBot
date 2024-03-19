e5a9af7577c787b767aeb60ce88f3f8e1c5dfed2
Fabian Wünderich <fabian@wuenderich.de>
2024-03-20 00:14:17 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.serialization") version "1.9.10"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
     java
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -17,23 +17,23 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.6.0")
+    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.6.3")
 
-    implementation("io.ktor", "ktor-client-core", "2.3.4")
+    implementation("io.ktor", "ktor-client-core", "2.3.9")
     implementation("io.ktor", "ktor-client-cio", "2.3.4")
-    implementation("io.ktor", "ktor-client-content-negotiation", "2.3.4")
-    implementation("io.ktor", "ktor-serialization-kotlinx-json", "2.3.4")
+    implementation("io.ktor", "ktor-client-content-negotiation", "2.3.9")
+    implementation("io.ktor", "ktor-serialization-kotlinx-json", "2.3.9")
 
-    implementation("org.jetbrains.exposed", "exposed-core", "0.44.0")
-    implementation("org.jetbrains.exposed", "exposed-dao", "0.44.0")
-    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.44.0")
-    implementation("org.jetbrains.exposed", "exposed-java-time", "0.44.0")
+    implementation("org.jetbrains.exposed", "exposed-core", "0.48.0")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.48.0")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.48.0")
+    implementation("org.jetbrains.exposed", "exposed-java-time", "0.48.0")
 
-    implementation("org.xerial", "sqlite-jdbc", "3.43.0.0")
+    implementation("org.xerial", "sqlite-jdbc", "3.45.2.0")
 
     implementation("dev.inmo", "tgbotapi", "9.1.2")
 
-    implementation("io.github.oshai", "kotlin-logging-jvm", "5.1.0")
+    implementation("io.github.oshai", "kotlin-logging-jvm", "6.0.3")
     implementation("org.apache.logging.log4j", "log4j-slf4j2-impl", "2.20.0")
 
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.9.2")
